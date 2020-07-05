fc35b1fdb7bfecb257d73768cb45779af868eba2
Nomaan <nomaanhusain@gmail.com>
2020-07-05 20:43:50 +0530

    commit

--- UbKsRodCutting.java
+++ UbKsRodCutting.java
@@ -11,11 +11,39 @@ lenarr={1,2,3,4}
 public class UbKsRodCutting {
     public static void main(String[] args) {
         int []profit=new int[]{6,4,6,7};
-        int L=2;
-        int rodLen[]=new int[L];
-        for(int i=1;i<=L;i++){
-            rodLen[i]=i;
+        int L=3;
+        int rodLen[]=new int[profit.length];
+        for(int i=0;i<profit.length;i++){
+            rodLen[i]=i+1;
         }
-        
+        int t[][]=new int[profit.length+1][L+1];
+        for(int i=0;i<=profit.length;i++){
+            for(int j=0;j<=L;j++){
+                if(i==0 || j==0){
+                    t[i][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<=profit.length;i++){
+            for(int j=1;j<=L;j++){
+                if(rodLen[i-1]<=j){
+            
+                        t[i][j]=Math.max(profit[i-1]+t[i][j-rodLen[i-1]],t[i-1][j]);
+                    }
+                else{
+                    t[i][j]=t[i-1][j];
+                }
+            }
+        }
+
+
+        //Printing
+        for(int i=0;i<=profit.length;i++){
+            for(int j=0;j<=L;j++){
+                System.out.print(t[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
