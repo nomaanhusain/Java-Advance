7bfcfa923bd093c56382e415ffb7014f93fba1ec
Nomaan <nomaanhusain@gmail.com>
2020-07-05 20:05:53 +0530

    commit

---
+++ UbKsRodCutting.java
@@ -0,0 +1,10 @@
+/*
+We have been given 2 arrays, one of length of rod, one of price(profit) for that length
+We have been given a certain rod of length, say L
+we have to cut L in such a way that we maximize our profit
+*/
+public class UbKsRodCutting {
+    public static void main(String[] args) {
+        
+    }
+}
