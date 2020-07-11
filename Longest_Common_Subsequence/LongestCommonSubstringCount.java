b6ccfe8994af22a9e678e79ab03bc403918805da
Nomaan <nomaanhusain@gmail.com>
2020-07-11 19:42:00 +0530

    commit

---
+++ Longest_Common_Subsequence/LongestCommonSubstringCount.java
@@ -0,0 +1,48 @@
+package Longest_Common_Subsequence;
+
+public class LongestCommonSubstringCount {
+    public static void main(String[] args) {
+        String a="acvtr";
+        String b="tyvtx";
+        char x[]=a.toCharArray();
+        char y[]=b.toCharArray();
+
+
+        int t[][]=new int[x.length+1][y.length+1];
+        for(int i=0;i<x.length;i++){
+            for(int j=0;j<y.length;j++){
+                if(i==0||j==0){
+                    t[i][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<x.length+1;i++){
+            for(int j=1;j<y.length+1;j++){
+                if(x[i-1]==y[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<=x.length;i++){
+            System.out.print(i+" --> ");
+            for(int j=0;j<=y.length;j++){
+                System.out.print(t[i][j]+" ");
+            }
+            System.out.println();
+        }
+        //For answer retrun max of matrix
+        int max=0;
+        for(int i=0;i<=x.length;i++){
+            for(int j=0;j<=y.length;j++){
+            if(t[i][j]>max){
+                max=t[i][j];
+            }
+            }
+        }
+        System.out.println("Answer --> "+max);
+    }
+}
