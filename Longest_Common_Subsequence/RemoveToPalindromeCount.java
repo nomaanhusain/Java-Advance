c4b71704e1b2c31a4e2db5043fd932f18a23d6bd
Nomaan <nomaanhusain@gmail.com>
2020-07-12 12:39:45 +0530

    commit

---
+++ Longest_Common_Subsequence/RemoveToPalindromeCount.java
@@ -0,0 +1,43 @@
+package Longest_Common_Subsequence;
+
+public class RemoveToPalindromeCount {
+    public static void main(String[] args) {
+        String a="agbcba";
+        String b="";
+        //Reverse the string
+        char[] temp = a.toCharArray(); 
+        for (int i = temp.length-1; i>=0; i--){
+            b=b+temp[i]; }
+            char x[]=a.toCharArray();
+            char y[]=b.toCharArray();
+            int t[][]=new int[x.length+1][y.length+1];
+            for(int i=0;i<x.length;i++){
+                for(int j=0;j<y.length;j++){
+                    if(i==0||j==0){
+                        t[i][j]=0;
+                    }
+                }
+            }
+    
+            for(int i=1;i<x.length+1;i++){
+                for(int j=1;j<y.length+1;j++){
+                    if(x[i-1]==y[j-1]){
+                        t[i][j]=1+t[i-1][j-1];
+                    }
+                    else{
+                        t[i][j]=Math.max(t[i][j-1], t[i-1][j]);
+                    }
+                }
+            }
+    
+            for(int i=0;i<=x.length;i++){
+                System.out.print(i+" --> ");
+                for(int j=0;j<=y.length;j++){
+                    System.out.print(t[i][j]+" ");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Number of elements to remove --> "+(a.length()-t[x.length][y.length]));
+    }
+}
