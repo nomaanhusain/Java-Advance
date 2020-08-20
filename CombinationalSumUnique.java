aa92c08e87fc359311e7f91bc1c447b80a1df72c
nomaanhusain <32061948+nomaanhusain@users.noreply.github.com>
2020-08-20 09:16:35 +0530

    Add files via upload

---
+++ CombinationalSumUnique.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationalSumUnique {
+    public static void main(String[] args) {
+        int target=7;
+        int []combination=new int[]{2,3,4,5,6,7};
+        List<List<Integer>> list=new ArrayList<>();
+        helper(list,new ArrayList<>(),target,0,combination);
+        System.out.println(list);
+    }
+    private static void helper( List<List<Integer>> list,List<Integer> curt,int target,int idx,int []comb){
+        if(target==0){
+            list.add(new ArrayList<>(curt));
+            return;
+        }
+        if(target<0){
+            return;
+        }
+        for(int i=idx;i<comb.length;i++){
+            curt.add(comb[i]);
+            helper(list, curt, target-comb[i], i, comb);
+            curt.remove(curt.size()-1);
+        }
+    }
+}
